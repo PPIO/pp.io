2b99e1ce5ad11e97c6c65639d11ff54ce5687328
ChangMM <1769111903@qq.com>
2019-01-10 11:41:35 +0800

    redesign the structrue projects page

--- app/pages/globalParam.js
+++ app/pages/globalParam.js
@@ -1,7 +1,7 @@
 module.exports = {
   indexUrl: process.env.NODE_ENV === 'production' ? 'https://www.pp.io' : '/',
   guideUrl: 'https://www.pp.io/docs/guide/',
-  apiUrl: 'https://www.pp.io/docs/api/',
+  apiUrl: 'https://www.pp.io/docs/',
   blogUrl: 'https://blog.pp.io',
   githubUrl: 'https://github.com/ppio',
   projectsUrl:
