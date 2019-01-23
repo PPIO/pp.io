ecf6b394a0f92c5f80349c79f760d9f3a31f2760
ChangMM <1769111903@qq.com>
2019-01-23 14:56:14 +0800

    add  & change resource url

--- app/components/index/download/render.js
+++ app/components/index/download/render.js
@@ -1,14 +1,5 @@
 module.exports = options => {
   const tpl = require('./download.ejs')
-  const transData = require('./translation.json')[options.lang]
-  return tpl({
-    isMob: options.isMob,
-    title: transData.title,
-    followTitle: transData.followTitle,
-    joinTitle: transData.joinTitle,
-    joinUs: transData.joinUs,
-    subscribe: transData.subscribe,
-    joinLink:
-      'https://gitter.im/PPIO/chat?utm_source=share-link&utm_medium=link&utm_campaign=share-link',
-  })
+  // const transData = require('./translation.json')[options.lang]
+  return tpl({})
 }
