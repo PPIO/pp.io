19cb11439fb533c8f86c7015302ca13a27679ada
ChangMM <1769111903@qq.com>
2019-02-18 21:48:46 +0800

    fix bug

--- sync2s3.sh
+++ sync2s3.sh
@@ -7,7 +7,7 @@ npm run build
 cp dist/index.html dist/index_mob.html
 
 # sync local files to S3:bucket
-aws s3 sync ./dist s3://pp.io --acl public-read
+aws s3 sync ./dist s3://pp.io2 --acl public-read
 
 # create cloudfront distribution invalidation
-aws cloudfront create-invalidation --distribution-id ECJMUYDIF91JL --paths /index.html  /download.html /index_mob.html
+aws cloudfront create-invalidation --distribution-id E1VRFX5KBKXI4R --paths /index.html  /download.html /index_mob.html
