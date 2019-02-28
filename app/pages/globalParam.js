4401787f52e5cd0be9580bfabbcca17bc65827be
ChangMM <1769111903@qq.com>
2019-02-28 15:58:56 +0800

    [dev] add advantage link

--- app/pages/globalParam.js
+++ app/pages/globalParam.js
@@ -3,7 +3,7 @@ module.exports = {
   guideUrl: 'https://www.pp.io/docs/guide/',
   apiUrl: 'https://www.pp.io/docs/',
   blogUrl: 'https://blog.pp.io',
-  forumUrl: 'https://www.reddit.com/r/ppio',
+  forumUrl: 'https://discord.gg/wAUED89',
   githubUrl: 'https://github.com/ppio',
   downloadUrl:
     process.env.NODE_ENV === 'production'
